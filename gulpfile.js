f6da6b97d7658b7e3b6b315c883963d3e1c673c2
Andreas Sander <andreas.sander@hotmail.de>
2016-12-10 11:06:16 +0100

    Gulp --production mode
    Adding a --production mode to gulp used during production deployment.
    
    Fixes #8.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,14 @@ var clean = require('gulp-clean');
 var path = require('path');
 var basePath = './themes/andi/src';
 
+/**
+ * Function returns true if gulp task is running in production mode
+ * @returns {boolean} 
+ */
+var isProductionMode = function () {
+  return process.argv.indexOf("--production") !== -1;
+};
+
 gulp.task('clear', function () {
   return gulp.src(
     path.join(
@@ -15,11 +23,14 @@ gulp.task('clear', function () {
 gulp.task('css', ['clear'], function () {
   var sass = require('gulp-sass');
   var sourcemaps = require('gulp-sourcemaps');
+  var cleanCSS = require('gulp-clean-css');
+  var gulpif = require('gulp-if');
   return gulp.src(path.resolve(path.join(basePath, 'sass'), '**/*.scss'))
     .pipe(sourcemaps.init())
     .pipe(sass({
       'includePaths': ['./node_modules/normalize-scss/sass/']
     }).on('error', sass.logError))
+    .pipe(gulpif(isProductionMode, cleanCSS()))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest(path.resolve(basePath, '../static/css')));
 });
