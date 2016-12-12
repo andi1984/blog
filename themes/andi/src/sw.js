ce5d3eb9352642fe1057b214bbbeaeade38864ff
Andreas Sander <andreas.sander@hotmail.de>
2016-12-12 23:57:36 +0100

    Service Workers - Simplification
    Only cache specific files at the moment.
    
    Todo: Check out a more sophisticated way of caching all internal responses with some proper cache busting.

--- themes/andi/src/sw.js
+++ themes/andi/src/sw.js
@@ -1,11 +1,7 @@
 // Cf. https://developers.google.com/web/fundamentals/getting-started/primers/service-workers
-var CACHE_NAME = 'Barnacle';
+var CACHE_NAME = 'Flightpath';
 var urlsToCache = [
   '/css/main.css'
-];
-var urlsToNotCache = [
-  '/sw-registration.js', // do not cache the service worker registration
-  '/manifest.webmanifest' // do not cache the manifest
 ];
 
 self.addEventListener('activate', function (event) {
@@ -45,40 +41,7 @@ self.addEventListener('fetch', function (event) {
         if (response) {
           return response;
         }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function (response) {
-            var blacklistMatch = urlsToNotCache.map(function (url) {
-              return event.request.url.indexOf(url) !== -1
-            }).some(function (urlNotToCacheFound) {
-              return urlNotToCacheFound;
-            });
-
-            // Check if we received a valid response which we are allowed to cache
-            if (!response || response.status !== 200 || response.type !== 'basic' || blacklistMatch) {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function (cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
+        return fetch(event.request);
       })
   );
 });
