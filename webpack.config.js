72353658cbd5c673aebc6fa7b8e0ce57c1ae6c41
Andreas Sander <andreas.sander@hotmail.de>
2017-11-12 21:29:20 +0100

    Workbox - Integrate workbox with webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,37 @@
-var path = require('path');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const workboxPlugin = require("workbox-webpack-plugin");
+const STATIC_FOLDER = path.resolve(__dirname, "themes", "andi", "static");
 
 module.exports = {
   entry: {
-    index: './themes/andi/src/js/index.js',
-    single: './themes/andi/src/js/single.js'
+    index: "./themes/andi/src/js/index.js",
+    single: "./themes/andi/src/js/single.js"
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'themes', 'andi', 'static')
+    filename: "[name].js",
+    path: STATIC_FOLDER
   },
   resolveLoader: {
-    modules: ['node_modules']
+    modules: ["node_modules"]
   },
-  plugins: [],
+  plugins: [
+    new workboxPlugin({
+      globDirectory: STATIC_FOLDER,
+      globPatterns: ["**/*.{html,js,css}"],
+      swDest: path.join(STATIC_FOLDER, "sw.js"),
+      clientsClaim: true,
+      skipWaiting: true
+    })
+  ],
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel-loader'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader"
+      }
+    ]
   }
 };
