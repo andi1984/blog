52dd4fccfdb5104ce77400621a3c8d9d34846b3a
Andreas Sander <andreas.sander@hotmail.de>
2018-03-17 23:37:33 +0100

    Spotify - Play! It is working!
    After back and forth it is working:
    
    It was necessary though to startup a HEROKU machine for the backend authentication as Spotify /api/token does not work from within browser. Thus when access token is finally given to the client from the Heroku machine, the rest is playing easily.
    
    Created a spotify helper to provide the necessary functions.

--- themes/andi/src/js/single.js
+++ themes/andi/src/js/single.js
@@ -1,3 +1,6 @@
-import {default as comments} from './comments';
-
+import { h, render } from "preact";
+import { default as comments } from "./comments";
+import Spotify from "./components/Spotify";
 comments();
+
+render(<Spotify />, document.querySelector('[data-js="spotify-play"]'));
