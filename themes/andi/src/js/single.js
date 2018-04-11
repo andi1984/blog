39cf7041bc7e27a05324571a3d5c726d3de0bc9c
Andreas Sander <andreas.sander@hotmail.de>
2018-04-11 13:03:19 +0200

    Remove Disqus Comments
    Remove Disqus comments due to DSGVO rules.

--- themes/andi/src/js/single.js
+++ themes/andi/src/js/single.js
@@ -1,7 +1,5 @@
 import { h, render } from "preact";
-import { default as comments } from "./comments";
 import Spotify from "./components/Spotify";
-comments();
 
 const spotifyPlayButton = document.querySelector('[data-js="spotify-play"]');
 if (spotifyPlayButton) {
