8a7333baef6feda46bd91c442bcff085c46b87d6
Andreas Sander <andreas.sander@hotmail.de>
2018-05-09 22:16:27 +0200

    Fix path in node script.

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 fs.writeFileSync(
-  "../.env",
+  "./.env",
   `SENTRY_KEY=${process.env.SENTRY_KEY}\nSPOTIFY_CLIENT_ID=${
     process.env.SPOTIFY_CLIENT_ID
   }\nSPOTIFY_API_ENDPOINT=${process.env.SPOTIFY_API_ENDPOINT}\n`
