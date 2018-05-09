c867ef30cb74ba68c8ed7607c0d8349db7f5fc38
Andreas Sander <andreas.sander@hotmail.de>
2018-05-09 22:29:54 +0200

    Remove demo alert.

--- themes/andi/src/js/sentry.js
+++ themes/andi/src/js/sentry.js
@@ -1,6 +1,5 @@
 import Raven from 'raven-js';
 
 export default function () {
-  alert(process.env.SENTRY_KEY);
   return Raven.config(`https://${process.env.SENTRY_KEY}@sentry.io/291445`).install();
 }
