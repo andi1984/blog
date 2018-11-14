c941a6290d88429a97f627af62a8f1511dd0fc3b
Andreas Sander <andreas.sander@hotmail.de>
2018-11-14 21:33:41 +0100

    Replace Sass by PostCSS
    Replace Sass stack by PostCSS plugins.

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,22 @@
+const path = require("path");
 module.exports = {
-  plugins: [require("autoprefixer")]
+  plugins: [
+    require("postcss-import"),
+    require("postcss-mixins")({
+      mixinsDir: path.join(__dirname, "src/css/mixins")
+    }),
+    require("postcss-functions")({
+      glob: path.join(__dirname, "src/css/functions", "*.js")
+    }),
+    require("postcss-nested"),
+    require("postcss-custom-properties")({
+      preserve: false
+    }),
+    require("postcss-custom-media")({
+      importFrom: [path.join(__dirname, "src/css/_viewport.media.css")]
+    }),
+    require('postcss-hexrgba'),
+    require('postcss-extend-rule'),
+    require("autoprefixer")
+  ]
 };
